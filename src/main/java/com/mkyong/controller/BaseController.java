9b7d0e49456a22bbc459c71db56e0f3f7948135f
HSarode-Compumatrice <hsarode@edrnet.com>
2017-05-10 19:42:49 +0530

    line no.11

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //cvlckfvlkf vlkfvk
 @Controller
-public class BaseController{
+public class BaseController11{
 
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
